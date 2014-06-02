2248bc95902c01c2441312f173de437fabd80d7e
pierre.h.garcia@gmail.com <pierre.h.garcia@gmail.com>
2014-06-02 18:01:53 +0200

    Change to gmail adress
    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ SolidareIt::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  ActionMailer::Base.default :from => 'noanswer@solidareit.be'
+  ActionMailer::Base.default :from => 'solidevit@gmail.com'
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
