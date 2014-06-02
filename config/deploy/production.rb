aa1d4a9d53be2d11486887b8f4c624270facf331
pierre.h.garcia@gmail.com <pierre.h.garcia@gmail.com>
2014-06-02 17:29:12 +0200

    Modify production variables
    Change production configuration:
    - Local deployment folder
    - config.action_mailer with the solidevit gmail address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,21 @@
 set :stage, :production
 
 set :rvm_type, :user
-set :deploy_to, '/home/solidareit/solidare-it/production'
+set :deploy_to, 'C:\Production'
 set :rails_env, "production"
-server 'jorisvh.net', user: 'solidareit', roles: %w{web app db}
+server 'heroku.com', user: 'pierre.h.garcia@gmail.com', roles: %w{web app db}
+
+config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "myapp.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
