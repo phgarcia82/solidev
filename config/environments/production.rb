6b9058e9b9981dd7df668ce0053c71498c43c824
pierre.h.garcia@gmail.com <pierre.h.garcia@gmail.com>
2014-06-02 18:36:26 +0200

    Send mail
    Send mail not working
    tests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,13 +96,13 @@ config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "solidev.herokuapp.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "solidev.herokuapp.com",
+  :authentication => "plain",
+  :enable_starttls_auto => true,
+  :user_name => ENV["GMAIL_USERNAME"],
+  :password => ENV["GMAIL_PASSWORD"]
 }
   
 end
