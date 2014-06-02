ffe2764388504c4a079aebc7d11cc1ade74b683a
pierre.h.garcia@gmail.com <pierre.h.garcia@gmail.com>
2014-06-02 18:18:40 +0200

    SMPT config
    C:\Users\phg.BVDEP\Documents\GitHub\solidev>heroku config:add
    GMAIL_USERNAME=sol
    idevit@gmail.com GMAIL_PASSWORD=solidareit84

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@ set :deploy_to, 'C:\Production'
 set :rails_env, "production"
 server 'heroku.com', user: 'pierre.h.garcia@gmail.com', roles: %w{web app db}
 
-config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'solidev.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
@@ -13,7 +13,7 @@ config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "myapp.herokuapp.com",
+  domain: "solidev.herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -68,16 +68,20 @@ SolidareIt::Application.configure do
   #config.action_mailer.default "Message-ID" => "<#{ UUID.generate }@xxxxxxx>"
 
   # ActionMailer configuration for GMail
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            'solidare.it.uclouvain',
-      password:             'bestteamever',
-      authentication:       'plain',
-      enable_starttls_auto: true  }
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "localhost:3000",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
+  
 end
